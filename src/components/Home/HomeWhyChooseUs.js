6361992d43e5231520c90a5722981bf14daa7df9
minhcuong99er <senseicuong@gmail.com>
2023-11-25 16:54:16 +0700

    update

---
+++ src/components/Home/HomeWhyChooseUs.js
@@ -0,0 +1,91 @@
+
+import React from "react";
+
+function HomeWhyChooseUs() {
+
+    return (
+        <>
+            <div className="one withsmallpadding ppb_header " style={{ textAlign: 'center', padding: '0px 0 0px 0', marginBottom: '40px' }}>
+                <div className="standard_wrapper">
+                    <div className="page_content_wrapper">
+                        <div className="inner">
+                            <div style={{ margin: 'auto', width: '100%' }}>
+                                <h2 className="ppb_title" style={{}}>Why Choose Us</h2>
+                                <div className="page_tagline" style={{}}>Here are reasons you should plan trip with us</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="standard_wrapper">
+                <div className="one_third withsmallpadding ppb_text" style={{ textAlign: 'center', padding: '0px 0 0px 0', marginBottom: '70px' }}>
+                    <div className="standard_wrapper">
+                        <div className="page_content_wrapper">
+                            <div className="inner">
+                                <div style={{ margin: 'auto', width: '100%' }}>
+                                    <p>
+                                        <img className="alignnone wp-image-3106 size-medium entered lazyloaded" src="https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/Map-Marker-300x300.png" width={150} height={150} alt="" data-lazy-src="https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/Map-Marker-300x300.png" data-ll-status="loaded" />
+                                        <noscript>&lt;img class="alignnone wp-image-3106 size-medium" src="https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/Map-Marker-300x300.png" width="150" height="150" alt="" /&gt;</noscript>
+                                    </p>
+                                    <h4 className="p1">
+                                        <span className="s1">
+                                            <b>Handpicked Hotels</b>
+                                        </span>
+                                    </h4>
+                                    <p>Lorem ipsum dolor sit amet, consect adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="standard_wrapper">
+                <div className="one_third withsmallpadding ppb_text" style={{ textAlign: 'center', padding: '0px 0 0px 0', marginBottom: '70px' }}>
+                    <div className="standard_wrapper">
+                        <div className="page_content_wrapper">
+                            <div className="inner">
+                                <div style={{ margin: 'auto', width: '100%' }}>
+                                    <p>
+                                        <img className="alignnone wp-image-3107 size-medium entered lazyloaded" src="https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/Worldwide-Location-300x300.png" width={150} height={150} alt="" data-lazy-src="https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/Worldwide-Location-300x300.png" data-ll-status="loaded" />
+                                        <noscript>&lt;img class="alignnone wp-image-3107 size-medium" src="https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/Worldwide-Location-300x300.png" width="150" height="150" alt="" /&gt;</noscript>
+                                    </p>
+                                    <h4 className="p1">
+                                        <span className="s1">
+                                            <b>World Class Service</b>
+                                        </span>
+                                    </h4>
+                                    <p>Lorem ipsum dolor sit amet, consect adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="standard_wrapper">
+                <div className="one_third last withsmallpadding ppb_text" style={{ textAlign: 'center', padding: '0px 0 0px 0', marginBottom: '70px' }}>
+                    <div className="standard_wrapper">
+                        <div className="page_content_wrapper">
+                            <div className="inner">
+                                <div style={{ margin: 'auto', width: '100%' }}>
+                                    <p>
+                                        <img className="alignnone wp-image-3108 size-medium entered lazyloaded" src="https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/Hot-Air-Balloon-300x300.png" width={140} height={140} alt="" data-lazy-src="https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/Hot-Air-Balloon-300x300.png" data-ll-status="loaded" />
+                                        <noscript>&lt;img class="alignnone wp-image-3108 size-medium" src="https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/Hot-Air-Balloon-300x300.png" width="140" height="140" alt="" /&gt;</noscript>
+                                    </p>
+                                    <h4 className="p1">
+                                        <span className="s1">
+                                            <b>Best Price Guarantee</b>
+                                        </span>
+                                    </h4>
+                                    <p>Lorem ipsum dolor sit amet, consect adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div data-bg="https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/pexels-photo-24377.jpg" className="parallax rocket-lazyload entered lazyloaded" style={{ height: '60vh', backgroundImage: 'url("https://themes-themegoods.b-cdn.net/grandtour/demo/wp-content/uploads/2016/12/pexels-photo-24377.jpg")' }} data-ll-status="loaded" />
+        </>
+    );
+}
+
+export default HomeWhyChooseUs;
--- src/view/Home.js
+++ src/view/Home.js
@@ -1,14 +1,24 @@
 
 import React from "react";
 import Header from "../layout/Header";
+import Footer from "../layout/Footer";
+import HomeArticles from "../components/Home/HomeArticles";
+import HomeWhyChooseUs from "../components/Home/HomeWhyChooseUs";
+import HomeBestValueTrips from "../components/Home/HomeBestValueTrips";
+import HomePopularDestinations from "../components/Home/HomePopularDestinations";
 
-function Home(props) {
+function Home() {
 
   return (
     <>
       <div id="wrapper" classname="hasbg transparent" style={{ paddingTop: 74 }}>
         <Header />
+        <HomePopularDestinations />
+        <HomeBestValueTrips />
+        <HomeWhyChooseUs />
+        <HomeArticles />
       </div>
+      <Footer />
     </>
   );
 }
